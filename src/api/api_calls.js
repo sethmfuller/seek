d6b7089881141a6f689165191c7a56ac54597466
Seth Fuller <semfulle@iupui.edu>
2018-05-05 12:19:10 -0400

    Built and deployed

--- src/api/api_calls.js
+++ src/api/api_calls.js
@@ -1,8 +1,12 @@
 const origin = "bible-api.com";
 
+const reqConfig = { method: 'GET',
+                    mode: 'no-cors', //or 'no-cors', or this field blank..
+                    cache: 'default' };
+
 const fetchData = (request) => {
     const req = new Request(`http://${origin}/${request}`, {
-        method: 'GET',
+        method: 'GET'
     });
 
     return returnGetData(req);
